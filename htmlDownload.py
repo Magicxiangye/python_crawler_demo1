9da7f75a2dcb1c02f7f03dceb881737728d2b6b3
Magicxiangye <30616925+Magicxiangye@users.noreply.github.com>
2021-06-20 23:40:45 +0800

    scrapy框架

--- htmlDownload.py
+++ htmlDownload.py
@@ -1,12 +1,14 @@
-import urllib.request#python2.7里urllib2拆分的两个库使用
+# python2.7以后urllib2拆分的两个库使用
+import urllib.request
 import urllib.error
-import re #分析正则库
-import chardet   #需要导入这个模块，检测编码格式
-import itertools #操作迭代对象的函数
-import urllib.parse #来创建url的绝对路径（url的解析，合并，编码，解码）
-import urllib.robotparser #还是python3的独特拆分
-from bs4 import BeautifulSoup#一个网页的解析模块（缺失的网页也可以解析）(用lxml模块库解析也可以，但是安装比较复杂)
+import re  # 分析正则库
+import chardet  # 需要导入这个模块，检测编码格式
+import itertools  # 操作迭代对象的函数
+import urllib.parse  # 来创建url的绝对路径（url的解析，合并，编码，解码）
+import urllib.robotparser  # 还是python3的独特拆分
+from bs4 import BeautifulSoup  # 一个网页的解析模块（缺失的网页也可以解析）(用lxml模块库解析也可以，但是安装比较复杂)
 from classMethod.ScrapeCallBack_Save import ScrapeCallBack
+
 
 # 从str到bytes:调用方法encode().
 # 编码是把Unicode字符串以各种方式编码成为机器能读懂的ASCII字符串
@@ -15,101 +17,111 @@ from classMethod.ScrapeCallBack_Save import ScrapeCallBack
 # python3中Unicode字符串是默认格式（就是str类型），
 # ASCII编码的字符串（就是bytes类型，bytes类型是包含字节值，其实不算是字符串，python3还有bytearray字节数组类型）要在前面加操作符b或B；
 # python2中则是相反的，ASCII编码字符串是默认，Unicode字符串要在前面加操作符u或U
-#encode和decode分别指编码和解码。
+# encode和decode分别指编码和解码。
 
-#分析爬取网页的特征的set集
-FIELDS = ('area','populattion','iso','country')
+# 分析爬取网页的特征的set集
+FIELDS = ('area', 'population', 'iso', 'country')
 
-#网页爬取下载(网页的下载函数)（没有重复下载的机制，遇到错误多试几次）#加入了可以使用代理的选项(有时间加上重新下载的机制，在下载类里已经有了)
-def download(url,user_agent='magicye12138',proxy=None,num_retries=2):
-    print('Downloading:',url)
-    headers = {'User-agent': user_agent}#请求头，字典类型(可以伪装成浏览器)
-    request = urllib.request.Request(url,headers=headers)
-    #当有代理时检查代理
+
+# 网页爬取下载(网页的下载函数)（没有重复下载的机制，遇到错误多试几次）#加入了可以使用代理的选项(有时间加上重新下载的机制，在下载类里已经有了)
+def download(url, user_agent='magicye12138', proxy=None, num_retries=2):
+    print('Downloading:', url)
+    # 请求头，字典类型(可以伪装成浏览器)
+    headers = {'User-agent': user_agent}
+    request = urllib.request.Request(url, headers=headers)
+    # 当有代理时检查代理
     opener = urllib.request.build_opener()
     if proxy:
-        proxy_params = {urllib.parse.urlparse(url).scheme: proxy}#url协议有默认的,没有的话加上--像'http'
+        proxy_params = {urllib.parse.urlparse(url).scheme: proxy}  # url协议有默认的,没有的话加上--像'http'
         opener.add_handler(urllib.request.ProxyHandler(proxy_params))
     try:
         html = urllib.request.urlopen(request).read()
     except urllib.error.URLError as e:
-        print('Download error',e.reason)
+        print('Download error', e.reason)
         html = None
 
     return html
 
-#网站地址爬虫
+
+# 网站地图爬虫
 def crawl_sitemap(url):
-    #先下载网页
+    # 先下载网页
     sitemap = download(url)
-    #解码(python3的编码转换)
+    # 解码(python3的编码转换)
     encode_type = chardet.detect(sitemap)
-    sitemap = sitemap.decode(encode_type['encoding'])#encode_type里的类型换成gbk格式的也可以转化为bytes类型
-    #用正则表达式分析链接
-    links = re.findall('<loc>(.*?)</loc>', sitemap)#re正则表达式
-    #下载获取到的URL
+    sitemap = sitemap.decode(encode_type['encoding'])  # encode_type里的类型换成gbk格式的也可以转化为bytes类型
+    # 用正则表达式分析链接
+    links = re.findall('<loc>(.*?)</loc>', sitemap)  # re正则表达式
+    # 下载获取到的URL
     for link in links:
         html = download(link)
         print(html)
 
-#遍历ID下载的爬虫
+
+# 遍历ID下载的爬虫
 def idDownload():
-    #itertools的count()创造无限迭代的函数根本停不下来只能按ctrl+c
-    max_error = 5 #最大的错误下载次数
-    num_error = 0 #当前到的错误次数
+    # itertools的count()创造无限迭代的函数根本停不下来只能按ctrl+c
+    max_error = 5  # 最大的错误下载次数
+    num_error = 0  # 当前到的错误次数
     for page in itertools.count(1):
-        url = 'http://example.webscraping.com/view/-%d' %page
+        url = 'http://example.webscraping.com/view/-%d' % page
         html = download(url)
         print(html)
         if html is None:
-            num_error +=1
+            num_error += 1
             if num_error == max_error:
                 break
         else:
-            #只要能下载就重置
+            # 只要能下载就重置
             num_error = 0
 
-#通过正则表达式来获取相对应的网页links
+
+# 通过正则表达式来获取相对应的网页links
 def get_links(html):
-    #返回从html中读取的link的list
-    #使用正则来确定要获取的link
-    webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']',re.IGNORECASE)
-    #返回相对应的list
+    # 返回从html中读取的link的list
+    # 使用正则来确定要获取的link
+    webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
+    # 返回相对应的list
     return webpage_regex.findall(html)
 
-#爬虫的回调函数(简单的只是把结果保存，类文件里是把爬取的数据保存到.csv的文件中)
-def scrape_callback(url,html):
-    if re.search('/view/',url):
-        soup = BeautifulSoup(html,"html.parser")
+
+# 爬虫的回调函数(简单的只是把结果保存，类文件里是把爬取的数据保存到.csv的文件中)
+def scrape_callback(url, html):
+    if re.search('/view/', url):
+        soup = BeautifulSoup(html, "html.parser")
         results = {}
         for field in FIELDS:
-            results[field] = soup.find('table').find('tr',id='places_%s_row'%field).find('td',class_='w2p_fw').text
+            results[field] = soup.find('table').find('tr', id='places_%s_row' % field).find('td', class_='w2p_fw').text
         print(results)
 
-#链接爬虫（这里还没更新成下载类的新的下载方法,标记一下）
-def link_crawler(seed_url,link_regex,user_agent='GoodCrawler',max_depth =2,scrape_callback=None):#有的时候代理名要换(爬虫陷阱功能要禁用的话，max_depth为负数就可以)
+
+# 链接爬虫（这里还没更新成下载类的新的下载方法,标记一下）
+def link_crawler(seed_url, link_regex, user_agent='GoodCrawler', max_depth=2,
+                 scrape_callback=None):  # 有的时候代理名要换(爬虫陷阱功能要禁用的话，max_depth为负数就可以)
     crawl_queue = [seed_url]
     print(crawl_queue)
-    #使用set表来计入无重复值的表单
+    # 使用set表来计入无重复值的表单
     seen = set(crawl_queue)
-    #爬虫陷阱功能中深度的定义的变量
+    # 爬虫陷阱功能中深度的定义的变量
     depth_dict = {}
     depth_dict[seed_url] = 0
-    #抓取回调的功能设置的List
+    # 抓取回调的功能设置的List
     links = []
     while crawl_queue:
+        # move list默认是数组中的最后一个元素，并返回对象
         url = crawl_queue.pop()
+        # 下载的深度
         depth = depth_dict[url]
-        #检查robotparser文件只爬取可以爬的文件
+        # 检查robotparser文件只爬取可以爬的文件
         rp = urllib.robotparser.RobotFileParser()
-        rp.set_url('http://example.webscraping.com/robots.txt')#设置要阅读的可爬取文件路径（绝对路径）
-        rp.read()#开始robots解析
-        if rp.can_fetch(user_agent,url):
+        rp.set_url('http://example.webscraping.com/robots.txt')  # 设置要阅读的可爬取文件路径（绝对路径）
+        rp.read()  # 开始robots解析
+        if rp.can_fetch(user_agent, url):
             html = download(url)
-            #记入网页的数据
+            # 记入网页的数据
             if scrape_callback:
-                links.extend(scrape_callback(url,html) or [])
-            #还得解码编码
+                links.extend(scrape_callback(url, html) or [])
+            # 还得解码编码
             encode_type = chardet.detect(html)
             html = html.decode(encode_type['encoding'])
             if depth != max_depth:
@@ -138,25 +150,25 @@ def NormalDataCatch():
         html)
     return output
 
-#解析网页的模块来爬取内容
+
+# 解析网页的模块来爬取内容
 def bs4MethodDataCatch():
     url = 'http://example.webscraping.com/places/default/view/Afghanistan-1'
     htmls = download(url)
-    soup = BeautifulSoup(htmls,"html.parser")#传入网页进行分析
-    #定位到要获取数据的位子
-    tr = soup.find(attrs={'id':'places_area__row'})#用来找这个tr的属性（常用id，因为唯一）
-    td = tr.find(attrs={'class':'w2p_fw'})#再定位一层
-    area = td.text#获取到内容
+    soup = BeautifulSoup(htmls, "html.parser")  # 传入网页进行分析
+    # 定位到要获取数据的位子
+    tr = soup.find(attrs={'id': 'places_area__row'})  # 用来找这个tr的属性（常用id，因为唯一）
+    td = tr.find(attrs={'class': 'w2p_fw'})  # 再定位一层
+    area = td.text  # 获取到内容
     return area
 
-#运行
+
+# 运行
 if __name__ == '__main__':
-   url = "http://example.webscraping.com"
-   link_regex = "/(index|places)/(index|default)/(index|view)"#地址后缀要写对
-   test = link_crawler(url,link_regex,scrape_callback=ScrapeCallBack())
-   print(test)
-   #正则表达式抓取网页的内容
-   # output = bs4MethodDataCatch()
-   # print(output)
-
-
+    url = "http://example.webscraping.com"
+    link_regex = "/(index|places)/(index|default)/(index|view)"  # 地址后缀要写对
+    test = link_crawler(url, link_regex, scrape_callback=ScrapeCallBack())
+    print(test)
+    # 正则表达式抓取网页的内容
+    # output = bs4MethodDataCatch()
+    # print(output)
--- search_context.py
+++ search_context.py
@@ -1,6 +1,9 @@
 # 抽取网页的模块
 import lxml.html
-# 之前的版本，cssselect直接在lxml中包含了，但是现在作为一个单独的模块出现了。因此需要单独安装。
+# 之前的版本，cssselect直接在lxml中包含了，但是现在作为一个单独的模块出现了。因此需要单独安装。=
+
+import json
+import string
 # import cssselect
 from classMethod.Downloader import Downloader
 
@@ -12,11 +15,55 @@ sys.path.append(rootPath)
 
 
 # 测试一下动态提取的功能
+#  了解网页是如何加载数据的（这个过程被称为逆向工程）
 if __name__ == "__main__":
     # 在爬取动态的网页的时候，没能爬取到当前的动态网页的情况，所以爬取的动态的内容为空
     D = Downloader()
-    html = D('http://example.webscraping.com/search')
+    # 下面下载的是带有AJAX的网页反馈的内容
+    # html = D('http://example.webscraping.com/ajax/search.json?page=0&page_size=10&search_term=a')
+    # 在这个网页的下载中，使用的是ajax连接的搜索
+    # page应该表示的是ajax库里总的页数
+    # 搜索的结果还会被分为多个页面
+    # 具体的网页的搜索的条件，是根据不同的网页来进行调整的
+    # 分为多少页是与page_size的大小有关系
+    # ajax响应返回的数据是JSON格式的，可以用
+    # python的JSON模块解析成为一个字典
+    # final_result = json.load(html)
     # 使用库来结构下载的网页的模型，来提取详细的信息
-    tree = lxml.html.fromstring(html)
-    result = tree.cssselect('div#results a')
-    print(result)
+    # tree = lxml.html.fromstring(html)
+    # result = tree.cssselect('div#results a')
+    # print(final_result)
+
+    # 动态的搜索动态网页的所有的查询结果
+    # 格式化的网页模板 format()的填充方式
+    template_url = 'http://example.webscraping.com/ajax/search.json?page={}&page_size=10&search_term={}'
+    contries = set()
+    # 这个网页，每一个字母就是一个搜索的条件
+    for letter in string.ascii_lowercase:
+        page = 0
+        while True:
+            html = D(template_url.format(page, letter))
+            try:
+                # 下载返回的是JSON格式的数据
+                ajax = json.load(html)
+            except ValueError as e:
+                print(e)
+                ajax = None
+            else:
+                for record in ajax['records']:
+                    # 加入字典一下，是为了去掉重复
+                    contries.add(record['country'])
+
+            page += 1
+            if ajax is None or page >= ajax['num_pages']:
+                break
+
+    # 生成的数据进行保存
+    open('countries.txt', 'w').write('\n'.join(sorted(contries)))
+    # 使用的边界条件，也可以更高效的继续宁查询，像是page_size提高显示的数量，也可以使得下载的次数减少
+    # 像是匹配的搜索的条件search_term使用正则表达式进行的搜索条件，可以不用进行循环
+    # 看网页的情况而定
+
+    # 也可以按行写入CSV文件中
+
+
